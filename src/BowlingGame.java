233766b51d7a3348098bec1a40acf5e53f57fd73
Yannouxt <laforest.yann@gmail.com>
2018-04-25 15:53:10 +0200

    Job done

--- src/BowlingGame.java
+++ src/BowlingGame.java
@@ -1,12 +1,65 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class BowlingGame {
 
     private int score;
+    
+    private static final int BASE_SCORE_SPARE_STRIKE = 10; 
+    
+    LinkedList<AbstractFrame> frames = new LinkedList<>();
+    
+    public BowlingGame(CasualFrame[] casualFrames, FinalFrame finalFrame) {
+    	int i = 0;
+    	while (i <= 8) {
+    		this.frames.addLast(casualFrames[i]);
+        	i++;
+    	}
 
-    public int score() {
+        this.frames.addLast(finalFrame);
+        	
+    	
+ 
+    	this.score = calculateThisScore();
+    }
+
+    
+    public int calculateThisScore() {
+    	int result = 0;
+    	LinkedList<AbstractFrame> analyzedFrames = this.getFrames();
+    	
+    	for (AbstractFrame frame : analyzedFrames) {
+    		if (!FrameDetail.FAULT.equals(frame.getFrameDetail())) {
+    		
+	    		if (FrameDetail.STRIKE.equals(frame.getFrameDetail())) {
+	    			result += BASE_SCORE_SPARE_STRIKE + frame.getThisNextFrameStrikeBonus(analyzedFrames);
+	    		}
+	    		else if (FrameDetail.SPARE.equals(frame.getFrameDetail())) {
+	    			result += BASE_SCORE_SPARE_STRIKE + frame.getThisNextFrameSpareBonus(analyzedFrames);
+	    		}
+	    		else {
+	    			result += frame.getFirstThrow().getKeelDownCount() + frame.getSecondThrow().getKeelDownCount();
+	    		}
+	    	}
+    	}
+    	return result;
+    }
+
+    public int getScore() {
         return score;
     }
 
-    public void roll(int numberPins) {
-        score += numberPins;
-    }
+	public LinkedList<AbstractFrame> getFrames() {
+		return frames;
+	}
+
+	public void setFrames(LinkedList<AbstractFrame> frames) {
+		this.frames = frames;
+	}
+
+	public void setScore(int score) {
+		this.score = score;
+	}
+
 }
--- src/BowlingGameTest.java
+++ src/BowlingGameTest.java
@@ -1,37 +1,121 @@
+
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
 import org.junit.Test;
-
-import static junit.framework.TestCase.assertEquals;
-
 
 public class BowlingGameTest {
 
-    BowlingGame game = new BowlingGame();
-
-    @Test
-    public void queDesGoutieresDonne0Points() {
-        rollMany(20, 0);
-        assertEquals(0, game.score());
+    BowlingGame game;
+    
+    @Before
+    public void clean() {
+    	this.game = null;
+    	CasualFrame[] casualFrames = {
+    		new CasualFrame(0, 0, ThrowDetail.NONE, ThrowDetail.NONE),
+    		new CasualFrame(0, 0, ThrowDetail.NONE, ThrowDetail.NONE),
+    		new CasualFrame(0, 0, ThrowDetail.NONE, ThrowDetail.NONE),
+    		new CasualFrame(0, 0, ThrowDetail.NONE, ThrowDetail.NONE),
+    		new CasualFrame(0, 0, ThrowDetail.NONE, ThrowDetail.NONE),
+    		new CasualFrame(0, 0, ThrowDetail.NONE, ThrowDetail.NONE),
+    		new CasualFrame(0, 0, ThrowDetail.NONE, ThrowDetail.NONE),
+    		new CasualFrame(0, 0, ThrowDetail.NONE, ThrowDetail.NONE),
+    		new CasualFrame(0, 0, ThrowDetail.NONE, ThrowDetail.NONE)
+    	};
+    	FinalFrame finalFrame = new FinalFrame(0, 0, 0, ThrowDetail.NONE, ThrowDetail.NONE, ThrowDetail.NONE);
+    	this.game = new BowlingGame(casualFrames, finalFrame);
     }
 
     @Test
-    public void uneSeuleQuille(){
-        game.roll(1);
-        rollMany(19, 0);
-        assertEquals(1, game.score());
+    public void queDesGoutieresDonne0Points() { 
+        assertThat(game.getScore(), is(0));
+    	assertThat(game.getFrames().size(), is(10));
     }
-
+    
     @Test
-    public void bonusDuSpare(){
-        game.roll(8);
-        game.roll(2);
-        game.roll(1);
-        rollMany(17, 0);
-        assertEquals(12, game.score());
+    public void deuxQuillesEtDesFautes(){
+    	game.getFrames().remove(0);
+    	game.getFrames().add(0, new CasualFrame(1, 0, ThrowDetail.NONE, ThrowDetail.NONE));
+    	
+    	game.getFrames().remove(3);
+    	game.getFrames().add(3, new CasualFrame(0, 1, ThrowDetail.NONE, ThrowDetail.NONE));
+    	
+    	game.getFrames().remove(5);
+    	game.getFrames().add(5, new CasualFrame(0, 1, ThrowDetail.FAULT, ThrowDetail.NONE));
+    	
+    	game.getFrames().remove(6);
+    	game.getFrames().add(6, new CasualFrame(0, 1, ThrowDetail.FAULT, ThrowDetail.NONE));
+    	
+        assertThat(game.calculateThisScore(), is(2));
+    	assertThat(game.getFrames().size(), is(10));
     }
-
-    private void rollMany(int numberRolls, int numberPins) {
-        for (int i = 0; i < numberRolls; i++) {
-            game.roll(numberPins);
-        }
+    
+    @Test
+    public void strikeDoubleEtSpareSuiviParFaute(){
+    	game.getFrames().remove(0);
+    	game.getFrames().add(0, new CasualFrame(10, 0, ThrowDetail.NONE, ThrowDetail.NONE));
+    	
+    	game.getFrames().remove(1);
+    	game.getFrames().add(1, new CasualFrame(10, 0, ThrowDetail.NONE, ThrowDetail.NONE));
+    	
+    	game.getFrames().remove(2);
+    	game.getFrames().add(2, new CasualFrame(7, 3, ThrowDetail.NONE, ThrowDetail.NONE));
+    	
+    	game.getFrames().remove(3);
+    	game.getFrames().add(3, new CasualFrame(1, 0, ThrowDetail.NONE, ThrowDetail.FAULT));
+    	
+    	/** (10+10) strike + (10+7+3) strike + (10 + 1) spare + 0 car faute*/
+        assertThat(game.calculateThisScore(), is(51));
+    	assertThat(game.getFrames().size(), is(10));
+    }
+    
+    @Test
+    public void avecUnLancerIncoherent(){
+    	game.getFrames().remove(0);
+    	game.getFrames().add(0, new CasualFrame(10, 0, ThrowDetail.NONE, ThrowDetail.NONE));
+    	
+    	game.getFrames().remove(1);
+    	game.getFrames().add(1, new CasualFrame(10, 0, ThrowDetail.NONE, ThrowDetail.NONE));
+    	
+    	game.getFrames().remove(2);
+    	game.getFrames().add(2, new CasualFrame(7, 3, ThrowDetail.NONE, ThrowDetail.NONE));
+    	
+    	game.getFrames().remove(3);
+    	game.getFrames().add(3, new CasualFrame(10, 0, ThrowDetail.FAULT, ThrowDetail.NONE));
+    	
+    	/** (10+10) strike + (10+7+3) strike + (10 + 0) spare + 0 car faute*/
+        assertThat(game.calculateThisScore(), is(50));
+    	assertThat(game.getFrames().size(), is(10));
+    }
+    
+    @Test
+    public void strikeSurLaDerniereFrame(){
+    	game.getFrames().remove(9);
+    	game.getFrames().addLast(new FinalFrame(10, 0, 10, ThrowDetail.NONE, ThrowDetail.NONE, ThrowDetail.NONE));
+    	
+    	assertThat(game.getFrames().size(), is(10));
+        assertThat(game.calculateThisScore(), is(20));
+    }
+    
+    @Test
+    public void strikeSurLaDerniereFrameMaisFaute3emeLancer(){
+    	game.getFrames().remove(9);
+    	game.getFrames().addLast(new FinalFrame(10, 0, 10, ThrowDetail.NONE, ThrowDetail.NONE, ThrowDetail.FAULT));
+    	
+    	assertThat(game.getFrames().size(), is(10));
+        assertThat(game.calculateThisScore(), is(10));
+        assertThat(game.getFrames().get(9), instanceOf(FinalFrame.class));
+    }
+    
+    @Test
+    public void spareSurLaDerniereFrame(){
+    	game.getFrames().remove(9);
+    	game.getFrames().addLast(new FinalFrame(6, 4, 8, ThrowDetail.NONE, ThrowDetail.NONE, ThrowDetail.NONE));
+    	
+    	assertThat(game.getFrames().size(), is(10));
+        assertThat(game.calculateThisScore(), is(18));
     }
 }
